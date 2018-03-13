e0ec80b16513de0a9d7a1a26769dc921fb2d755a
Eladio Jose Abquina <eladio@getpantheon.com>
2018-03-13 07:11:33 +0000

    Caldera ReCaptcha Add-on

--- wp-content/themes/cactus/header.php
+++ wp-content/themes/cactus/header.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <?php wp_head(); ?>
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <?php
 	$body_class = 'page blog';
